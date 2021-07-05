11895216b37c51b67e4c1806d62587c595c43bef
Dario Rodrigues de Melo <dariorodriguesdr@gmail.com>
2021-07-05 18:42:31 -0300

    App Hello_World

---
+++ APP_01_Hello_world/app/src/main/java/com/Noobs/hello_world/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.Noobs.hello_world
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
