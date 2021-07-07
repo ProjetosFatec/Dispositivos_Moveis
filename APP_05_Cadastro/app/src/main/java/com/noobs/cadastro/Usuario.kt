6ddbf0cf3677864696fb586247313511f79034de
Dario Rodrigues de Melo <dariorodriguesdr@gmail.com>
2021-07-07 17:58:29 -0300

    alterando APP_5 adicionando classe kotlin

--- APP_05_Cadastro/app/src/main/java/com/noobs/cadastro/Usuario.kt
+++ APP_05_Cadastro/app/src/main/java/com/noobs/cadastro/Usuario.kt
@@ -4,4 +4,4 @@ import android.os.Parcelable
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
-data class Cliente(val nome: String, val idade: Int) : Parcelable
+data class Usuario(val nome: String, val idade: Int) : Parcelable
