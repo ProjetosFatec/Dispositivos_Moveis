34b6879dbde4688e41bcf22bd4c462dba692eb73
Dario Rodrigues de Melo <dariorodriguesdr@gmail.com>
2021-07-08 17:37:39 -0300

    início do APP_08

---
+++ APP_08_Listagem_de_pets/app/src/main/java/com/noobs/listagem_de_pets/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.noobs.listagem_de_pets
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
