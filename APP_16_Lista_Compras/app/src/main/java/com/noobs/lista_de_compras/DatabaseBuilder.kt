cdb7b8cadc829b59a3979add462ec532408aaf9e
Dario Rodrigues de Melo <dariorodriguesdr@gmail.com>
2021-07-09 11:02:36 -0300

    App 16 layout item_tarefa.xml

--- APP_16_Lista_Compras/app/src/main/java/com/noobs/lista_de_compras/DatabaseBuilder.kt
+++ APP_16_Lista_Compras/app/src/main/java/com/noobs/lista_de_compras/DatabaseBuilder.kt
@@ -1,4 +1,19 @@
 package com.noobs.lista_de_compras
 
-class DatabaseBuilder {
+import android.content.Context
+import androidx.room.Room
+
+object DatabaseBuilder {
+    private var instancia: AppDatabase?=null
+
+    fun getAppDatabase(context: Context): AppDatabase?{
+        if (instancia==null){
+            synchronized(AppDatabase::class){
+                instancia = Room.databaseBuilder(context.applicationContext, AppDatabase::class.java, "AppDb")
+                    .fallbackToDestructiveMigration()
+                    .build()
+            }
+        }
+        return instancia
+    }
 }
