ab982bf8e018048f63909eeaece46904747311b7
Dario Rodrigues de Melo <dariorodriguesdr@gmail.com>
2021-07-10 12:59:39 -0300

    Enum senioridade

---
+++ APP_10_Lista_Programadores/app/src/main/java/com/noobs/lista_programadores/Senioridade.kt
@@ -0,0 +1,8 @@
+package com.noobs.lista_programadores
+package com.victor.app10_cadastroprogramadores
+
+enum class Senioridade(val nome: String) {
+    JUNIOR("Junior"),
+    PLENO("Pleno"),
+    SENIOR("Senior")
+}
