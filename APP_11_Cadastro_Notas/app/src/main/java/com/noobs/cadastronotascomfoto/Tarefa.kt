e543ba609c9c1bd3688b1948c6aad76080060dbd
Dario Rodrigues de Melo <dariorodriguesdr@gmail.com>
2021-07-10 16:26:48 -0300

    arrumando app_011

--- APP_11_Cadastro_Notas/app/src/main/java/com/noobs/cadastronotascomfoto/Tarefa.kt
+++ APP_11_Cadastro_Notas/app/src/main/java/com/noobs/cadastronotascomfoto/Tarefa.kt
@@ -1,4 +1,11 @@
 package com.noobs.cadastronotascomfoto
 
-class Tarefa {
-}
+import android.graphics.Bitmap
+
+class Tarefa (
+    var foto: Bitmap?=null,
+    var descricao: String,
+    var prioridade: Prioridade,
+    var perfil: Perfil,
+    var urgente: Boolean
+)
